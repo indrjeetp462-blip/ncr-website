d9282a45c04297357c849076ab482a5a447831b3
Indrajeet singh <indrjeetp462@gmail.com>
2026-09-10 01:42:03 +0530

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: 'https://ncr-website.vercel.app', lastModified: new Date() },
+    { url: 'https://ncr-website.vercel.app/services', lastModified: new Date() },
+    { url: 'https://ncr-website.vercel.app/about', lastModified: new Date() },
+    { url: 'https://ncr-website.vercel.app/contact', lastModified: new Date() },
+  ]
+}
